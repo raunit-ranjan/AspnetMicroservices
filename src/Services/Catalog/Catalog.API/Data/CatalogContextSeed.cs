0ac4906fc2130f2f016618555209807d67485f44
raunit-ranjan <raunitranjan@gmail.com>
2023-04-13 17:56:51 +0530

    Catalog Controller

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,36 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (!existProduct) 
+            {
+                productCollection.InsertManyAsync(GetPreconfigureProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetPreconfigureProducts()
+        {
+            return new List<Product>
+            {
+                new Product()
+                {
+                    Id = "1249817kaja987987qwwqee9089",
+                    Name = "Samsung 10",
+                    Summary = "This is Samsung 10",
+                    Description = "Description",
+                    ImageFile = "product-1.png",
+                    Price = 950.00M,
+                    Category = "Smart Phone"
+                }
+            };
+
+        
+        }
+    }
+}
